c6518fd610d18723f44391a91941df502e547bfa
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-12 12:36:27 -0300

    adicionando break

--- lotomania.py
+++ lotomania.py
@@ -30,3 +30,4 @@ while total <= quantidade:
             """ Conta quantos números foram adicionados. """
         if contagem >= 50:
             """ Verifica se o número de números adicionados é igual a 50. """
+            break
